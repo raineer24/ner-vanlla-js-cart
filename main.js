c4159a92d73cc46cca9a1fc6f4071402c2bb219d
Raineer <delaritaraineer81@gmail.com>
2019-03-22 22:57:23 +0800

    querySelector product img name price

--- main.js
+++ main.js
@@ -3,11 +3,14 @@
 const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
 addToCartButtonsDOM.forEach(addToCartButtonsDOM => {
     addToCartButtonsDOM.addEventListener('click', () => {
-        console.log(product.querySelector('.product__image'));
-    console.log(product.querySelector('.product__name'));
-    console.log(product.querySelector('.product__price'));
+       const productDOM = addToCartButtonsDOM.parentNode;
+       const product = {
+           image: productDOM.querySelector('.product__image').getAttribute('src'),
+           name: productDOM.querySelector('.product__name').innerText,
+           price: productDOM.querySelector('.product__price').innerText,
+       };
+       console.log(product);
     });
-    const product = addToCartButtonsDOM.parentNode;
-    });
+});
 
 
