a7927ba3a5ac66b5082aa53b9afd9914e46d0ffa
Raineer <delaritaraineer81@gmail.com>
2019-03-22 22:24:35 +0800

    click addeventlistener

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 'use strict';
 
-const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TOC_CART"]');
-console.log(addToCartButtonsDOM);
+const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
+addToCartButtonsDOM.forEach(addToCartButtonsDOM => {
+    addToCartButtonsDOM.addEventListener('click', () => {
+        console.log(product.querySelector('.product__image'));
+    console.log(product.querySelector('.product__name'));
+    console.log(product.querySelector('.product__price'));
+    });
+    const product = addToCartButtonsDOM.parentNode;
+    });
+
+
