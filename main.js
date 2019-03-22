d30edca921d9822614d82ff090220ffb6b374862
Raineer <delaritaraineer81@gmail.com>
2019-03-23 00:19:48 +0800

    add to cart button function

--- main.js
+++ main.js
@@ -1,16 +1,23 @@
 'use strict';
 
+const cartDOM = document.querySelector('.cart');
+
 const addToCartButtonsDOM = document.querySelectorAll('[data-action="ADD_TO_CART"]');
-addToCartButtonsDOM.forEach(addToCartButtonsDOM => {
-    addToCartButtonsDOM.addEventListener('click', () => {
-       const productDOM = addToCartButtonsDOM.parentNode;
-       const product = {
-           image: productDOM.querySelector('.product__image').getAttribute('src'),
-           name: productDOM.querySelector('.product__name').innerText,
-           price: productDOM.querySelector('.product__price').innerText,
-       };
-       console.log(product);
+addToCartButtonsDOM.forEach(addToCartButtonDOM => {
+    addToCartButtonDOM.addEventListener('click', () => {
+       const productDOM = addToCartButtonDOM.parentNode;
+        const product = {
+            image: productDOM.querySelector('.product__image').getAttribute('src'),
+            name: productDOM.querySelector('.product__name').innerText,
+            price: productDOM.querySelector('.product__price').innerText,
+        };
+        cartDOM.insertAdjacentHTML('beforeend',
+            `
+            <div class="cart__item">
+               <img class="cart__item__image" src="${product.image}" alt="${product.name}">
+               <h3 class="class__item__name">${product.name}</h3>
+               <h3 class="class__item__price">${product.price}</h3>
+            </div>
+       `);
     });
-});
-
-
+}); 
