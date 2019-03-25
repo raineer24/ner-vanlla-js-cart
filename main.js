c956f362762d47dab351082378139660ac1e9523
Raineer <delaritaraineer81@gmail.com>
2019-03-26 06:15:35 +0800

    email change

--- main.js
+++ main.js
@@ -156,7 +156,7 @@ function checkout() {
     <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
         <input type="hidden" name="cmd" value="_cart">
         <input type="hidden" name="upload" value="1">
-        <input type="hidden" name="business" value="seller@dezignerfotos.com">
+        <input type="hidden" name="business" value="delaritaraineer81@gmail.com">
         <input type="hidden" name="item_name_1" value="Item Name 1">
         <input type="hidden" name="amount_1" value="1.00">
         <input type="hidden" name="shipping_1" value="1.75">
