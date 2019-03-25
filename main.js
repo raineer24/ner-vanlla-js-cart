ede9cf54ccaf37de693f670e01674168cfb2306b
Raineer <delaritaraineer81@gmail.com>
2019-03-26 06:19:08 +0800

    Join value

--- main.js
+++ main.js
@@ -157,15 +157,20 @@ function checkout() {
         <input type="hidden" name="cmd" value="_cart">
         <input type="hidden" name="upload" value="1">
         <input type="hidden" name="business" value="delaritaraineer81@gmail.com">
-        <input type="hidden" name="item_name_1" value="Item Name 1">
-        <input type="hidden" name="amount_1" value="1.00">
-        <input type="hidden" name="shipping_1" value="1.75">
-        <input type="hidden" name="item_name_2" value="Item Name 2">
-        <input type="hidden" name="amount_2" value="2.00">
-        <input type="hidden" name="shipping_2" value="2.50">
-        <input type="submit" value="PayPal">
-    </form>
+        `;
+
+        // <input type="hidden" name="item_name_1" value="Item Name 1">
+        // <input type="hidden" name="amount_1" value="1.00">
+        // <input type="hidden" name="shipping_1" value="1.75">
+        // <input type="hidden" name="item_name_2" value="Item Name 2">
+        // <input type="hidden" name="amount_2" value="2.00">
+        // <input type="hidden" name="shipping_2" value="2.50">
+
+        paypalFormHTML += `
+            <input type="submit" value="PayPal">
+            </form>
     `;
+    
 }
 function countCartTotal() {
     let cartTotal = 0;
